2599b6c66ee35a24a4b68793bda51c1b8ef3099f
Jefferson <swrt_9950@hotmail.com>
2015-06-11 17:44:38 +0200

    PCA now working.

--- src/pca.js
+++ src/pca.js
@@ -1,6 +1,7 @@
 'use strict';
 var Matrix = require('ml-matrix');
 var Stat = require('ml-stat');
+var SVD = Matrix.DC.SVD;
 
 module.exports = function pca(dataset) {
 
@@ -9,10 +10,20 @@ module.exports = function pca(dataset) {
         var std = Matrix.rowVector(Stat.matrix.standardDeviation(dataset, means, true));
         means = Matrix.rowVector(means);
 
-        var result = dataset.clone().addRowVector(means.neg());
+        var result = dataset.addRowVector(means.neg());
         return result.divRowVector(std);
     };
 
-    console.log(featureNormalize(dataset));
 
+    var normalizedDataset = featureNormalize(dataset.clone());
+    var covarianceMatrix = normalizedDataset.transpose().mmul(normalizedDataset).divS(dataset.rows);
+
+    var target = new SVD(covarianceMatrix, {
+        computeLeftSingularVectors: true,
+        computeRightSingularVectors: true,
+        autoTranspose: false
+    });
+
+    return {U: target.leftSingularVectors, S: target.diagonal};
 };
+
