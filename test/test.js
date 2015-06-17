6572c6a4e5e58601f5f783b8836d0e1f371efb74
Jefferson <swrt_9950@hotmail.com>
2015-06-17 09:51:25 +0200

    Variance explained test added.

--- test/test.js
+++ test/test.js
@@ -54,26 +54,31 @@ describe('PCA algorithm', function () {
                         [1.91895045046187, 5.07107847507096],
                         [3.95524687147485, 4.50532709674253],
                         [5.11795499426461, 6.08507386392396]];
+    var pca = new PCA(Matrix(testDataset));
 
     it('PCA Main test', function () {
-        var result = new PCA(Matrix(testDataset));
         var U = [ [0.7071, 0.7071], [0.7071, -0.7071]];
         var S = [ 1.70081, 0.25918];
 
         for(var i = 0; i < 2; ++i) {
             for(var j = 0; j < 2; ++j) {
-                (result.U[i][j]).should.be.approximately(U[i][j], 1e-3);
+                (pca.U[i][j]).should.be.approximately(U[i][j], 1e-3);
             }
         }
 
         for(i = 0; i < 2; ++i) {
-            (result.S[i]).should.be.approximately(S[i], 1e-3);
+            (pca.S[i]).should.be.approximately(S[i], 1e-3);
         }
     });
 
     it('Projection method', function () {
-        var pca = new PCA(Matrix(testDataset));
         var result = pca.project(testDataset, 1);
         result[0][0].should.be.approximately(-1.481274, 1e-5);
     });
+
+    it('Variance explained method', function () {
+        var varianceExplained = pca.getExplainedVariance();
+        varianceExplained[0].should.be.approximately(0.8677, 1e-4);
+        varianceExplained[1].should.be.approximately(0.1322, 1e-4);
+    })
 });
