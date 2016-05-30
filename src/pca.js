6b0d157144ed4324d33ca046995ad75e7a011c47
Michaël Zasso <targos@protonmail.com>
2016-05-30 17:13:02 +0200

    class + standard ml interface

--- src/pca.js
+++ src/pca.js
@@ -1,34 +1,97 @@
 'use strict';
-var Matrix = require('ml-matrix');
-var Stat = require('ml-stat');
-var SVD = Matrix.DC.SVD;
 
-module.exports = PCA;
+const Matrix = require('ml-matrix');
+const SVD = Matrix.DC.SVD;
+const Stat = require('ml-stat');
 
-/**
-* Creates new PCA (Principal Component Analysis) from the dataset
-* @param {Matrix} dataset
-* @param {Object} options - options for the PCA algorithm
-* @param {boolean} reload - for load purposes
-* @param {Object} model - for load purposes
-* @constructor
-* */
-function PCA(dataset, options, reload, model) {
+const defaultOptions = {
+    standardize: false
+};
 
-    if (reload) {
-        this.U = model.U;
-        this.S = model.S;
-        this.means = model.means;
-        this.std = model.std;
-        this.standardize = model.standardize
-    } else {
-        if(options === undefined) {
-            options = {
-                standardize: false
-            };
+class PCA {
+    /**
+     * Creates new PCA (Principal Component Analysis) from the dataset
+     * @param {Matrix} dataset
+     * @param {Object} options - options for the PCA algorithm
+     * @param {boolean} reload - for load purposes
+     * @param {Object} model - for load purposes
+     * @constructor
+     * */
+    constructor(dataset, options, reload, model) {
+        if (reload) {
+            this.U = model.U;
+            this.S = model.S;
+            this.means = model.means;
+            this.std = model.std;
+            this.standardize = model.standardize
+        } else {
+            options = Object.assign({}, defaultOptions, options);
+
+            this.standardize = !!options.standardize;
+
+            if (!Matrix.isMatrix(dataset)) {
+                dataset = new Matrix(dataset);
+            } else {
+                dataset = dataset.clone();
+            }
+
+            var normalization = adjust(dataset, this.standardize);
+            var normalizedDataset = normalization.result;
+
+            var covarianceMatrix = normalizedDataset.transpose().mmul(normalizedDataset).divS(dataset.rows);
+
+            var target = new SVD(covarianceMatrix, {
+                computeLeftSingularVectors: true,
+                computeRightSingularVectors: true,
+                autoTranspose: false
+            });
+
+            this.U = target.leftSingularVectors;
+            this.S = target.diagonal;
+            this.means = normalization.means;
+            this.std = normalization.std;
         }
+    }
 
-        this.standardize = options.standardize;
+    /**
+     * Load a PCA model from JSON
+     * @oaram {Object} model
+     * @return {PCA}
+     */
+    static load(model) {
+        if (model.name !== 'PCA')
+            throw new RangeError('Invalid model: ' + model.name);
+        return new PCA(null, null, true, model);
+    }
+
+    /**
+     * Exports the current model to an Object
+     * @return {Object} model
+     */
+    toJSON() {
+        return {
+            name: 'PCA',
+            U: this.U,
+            S: this.S,
+            means: this.means,
+            std: this.std,
+            standardize: this.standardize
+        };
+    }
+
+    /**
+     * Projects the dataset into new space of k dimensions,
+     * this method doesn't modify your dataset.
+     * @param {Matrix} dataset.
+     * @param {Number} k - dimensions to project.
+     * @return {Matrix} dataset projected in k dimensions.
+     * @throws {RangeError} if k is larger than the number of eigenvector
+     *                      of the model.
+     */
+    project(dataset, k) {
+        var dimensions = k - 1;
+        if (k > this.U.columns)
+            throw new RangeError("the number of dimensions must not be larger than " + this.U.columns);
 
         if (!Matrix.isMatrix(dataset)) {
             dataset = new Matrix(dataset);
@@ -36,114 +99,39 @@ function PCA(dataset, options, reload, model) {
             dataset = dataset.clone();
         }
 
-        var normalization = adjust(dataset, this.standardize);
-        var normalizedDataset = normalization.result;
+        var X = adjust(dataset, this.standardize, this.means, this.std).result;
+        return X.mmul(this.U.subMatrix(0, this.U.rows - 1, 0, dimensions));
+    }
 
-        var covarianceMatrix = normalizedDataset.transpose().mmul(normalizedDataset).divS(dataset.rows);
+    /**
+     * Returns the percentage variance of each eigenvector.
+     * @return {Number}
+     */
+    getExplainedVariance() {
+        var sum = this.S.reduce((previous, value) => previous + value);
+        return this.S.map(value => value / sum);
+    }
 
-        var target = new SVD(covarianceMatrix, {
-            computeLeftSingularVectors: true,
-            computeRightSingularVectors: true,
-            autoTranspose: false
-        });
+    /**
+     * Returns the Eigenvectors of the covariance matrix.
+     * @returns {Matrix}
+     */
+    getEigenvectors() {
+        return this.U;
+    }
 
-        this.U = target.leftSingularVectors;
-        this.S = target.diagonal;
-        this.means = normalization.means;
-        this.std = normalization.std;
+    /**
+     * Returns the Eigenvalues (on the diagonal).
+     * @returns {*}
+     */
+    getEigenvalues() {
+        return this.S;
     }
 }
 
-/**
-* Load a PCA model from JSON
-* @oaram {Object} model
-* @return {PCA}
-* */
-PCA.load = function (model) {
-    if(model.modelName !== 'PCA')
-        throw new RangeError("The current model is invalid!");
-
-    return new PCA(null, null, true, model);
-};
-
-/**
-* Exports the current model to an Object
-* @return {Object} model
-* */
-PCA.prototype.export = function () {
-    return {
-        modelName: "PCA",
-        U: this.U,
-        S: this.S,
-        means: this.means,
-        std: this.std,
-        standardize: this.standardize
-    };
-};
-
-/**
-* Function that project the dataset into new space of k dimensions,
-* this method doesn't modify your dataset.
-* @param {Matrix} dataset.
-* @param {Number} k - dimensions to project.
-* @return {Matrix} dataset projected in k dimensions.
-* @throws {RangeError} if k is larger than the number of eigenvector
-*                      of the model.
-* */
-PCA.prototype.project = function (dataset, k) {
-    var dimensions = k - 1;
-    if(k > this.U.columns)
-        throw new RangeError("the number of dimensions must not be larger than " + this.U.columns);
-
-    if (!Matrix.isMatrix(dataset)) {
-        dataset = new Matrix(dataset);
-    } else {
-        dataset = dataset.clone();
-    }
-
-    var X = adjust(dataset, this.standardize).result;
-    return X.mmul(this.U.subMatrix(0, this.U.rows - 1, 0, dimensions));
-};
-
-/**
-* This method returns the percentage variance of each eigenvector.
-* @return {Number} percentage variance of each eigenvector.
-* */
-PCA.prototype.getExplainedVariance = function () {
-    var sum = this.S.reduce(function (previous, value) {
-        return previous + value;
-    });
-    return this.S.map(function (value) {
-        return value / sum;
-    });
-};
-
-/**
- * Function that returns the Eigenvectors of the covariance matrix.
- * @returns {Matrix}
- */
-PCA.prototype.getEigenvectors = function () {
-    return this.U;
-};
-
-/**
- * Function that returns the Eigenvalues (on the diagonal).
- * @returns {*}
- */
-PCA.prototype.getEigenvalues = function () {
-    return this.S;
-};
-
-/**
-* This method returns a dataset normalized in the following form:
-* X = (X - mean) / std
-* @param dataset.
-* @param {Boolean} standarize - do standardization
-* @return A dataset normalized.
-* */
-function adjust(dataset, standarize) {
-    var means = Stat.matrix.mean(dataset);
-    var std = standarize ? Stat.matrix.standardDeviation(dataset, means, true) : undefined;
+function adjust(dataset, standarize, means, std) {
+    if (!means) means = Stat.matrix.mean(dataset);
+    if (!std) std = standarize ? Stat.matrix.standardDeviation(dataset, means, true) : undefined;
 
     var result = dataset.subRowVector(means);
     return {
@@ -152,3 +140,5 @@ function adjust(dataset, standarize) {
         std: std
     }
 }
+
+module.exports = PCA;
